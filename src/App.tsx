8f5715e8ceb0133e124c69e4e883cf51881eb1d9
keillaarr <keillaarruda9@gmail.com>
2025-11-04 22:31:25 -0300

    construção de site

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1 style={{marginTop: "200px"}}>testando página...</h1>
+    </>
+  );
+}
+
+export default App;
